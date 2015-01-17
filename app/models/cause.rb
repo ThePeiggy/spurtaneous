86434495cca2ce66625f55e139a4365fb1ad8443
Peihong Li <peihong.li@shopify.com>
2015-01-17 04:23:58 -0500

    added hit behavior

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,4 +1,12 @@
 class Cause < ActiveRecord::Base
 	belongs_to :user
 	validates :name, presence: true
+
+	def add_hit
+		update_attribute(hits: hits + 1)
+	end
+
+	def money_raised
+		hits * unit_donation
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :causes, dependent: :destroy
   validates :email, presence: true
+
+
+  def hit(cause)
+  	cause.add_hit
+  	#take money out
+  end
 end
